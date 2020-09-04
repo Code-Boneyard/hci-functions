4084d890cb87a2243db9688714811c24e70c25cf
Max Geller <mxgeller@gmail.com>
2020-09-03 19:50:06 -0700

    updating

--- functions/index.ts
+++ functions/index.ts
@@ -1,61 +1,33 @@
+const functions = require('firebase-functions');
 const algoliasearch = require('algoliasearch');
-const dotenv = require('dotenv');
-const firebase = require('firebase');
 
-// load values from the .env file in this directory into process.env
-dotenv.config();
+const APP_ID = functions.config().algolia.app;
+const ADMIN_KEY = functions.config().algolia.key;
 
-// configure firebase
-firebase.initializeApp({
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-});
-const database = firebase.database();
-
-// configure algolia
-const algolia = algoliasearch(
-    process.env.ALGOLIA_APP_ID,
-    process.env.ALGOLIA_API_KEY
-);
-const index = algolia.initIndex(process.env.ALGOLIA_INDEX_NAME);
+const client = algoliasearch(APP_ID, ADMIN_KEY);
+const index = client.initIndex('projects');
 
 
+exports.addToIndex = functions.firestore.document('projects/{projectId}')
+
+    .onCreate(snapshot => {
+
+        const data = snapshot.data();
+        const objectID = snapshot.id;
+
+        return index.addObject({ ...data, objectID });
+
+    });
 
 
-// Get all projects from Firebase
-const projectsRef = database.ref('/projects');
-projectsRef.on('child_added', addOrUpdateIndexRecord);
-projectsRef.on('child_changed', addOrUpdateIndexRecord);
-projectsRef.on('child_removed', deleteIndexRecord);
+exports.updateIndex = functions.firestore.document('projects/{projectId}')
 
-function addOrUpdateIndexRecord(project) {
-    // Get Firebase object
-    const record = project.val();
-    // Specify Algolia's objectID using the Firebase object key
-    record.objectID = project.key;
-    // Add or update object
-    index
-        .saveObject(record)
-        .then(() => {
-            console.log('Firebase object indexed in Algolia', record.objectID);
-        })
-        .catch(error => {
-            console.error('Error when indexing project into Algolia', error);
-            process.exit(1);
-        });
-}
+    .onUpdate((change) => {
+        const newData = change.after.data();
+        const objectID = change.after.id;
+        return index.saveObject({ ...newData, objectID });
+    });
 
-function deleteIndexRecord({ key }) {
-    // Get Algolia's objectID from the Firebase object key
-    const objectID = key;
-    // Remove the object from Algolia
-    index
-        .deleteObject(objectID)
-        .then(() => {
-            console.log('Firebase object deleted from Algolia', objectID);
-        })
-        .catch(error => {
-            console.error('Error when deleting project from Algolia', error);
-            process.exit(1);
-        });
-}
+exports.deleteFromIndex = functions.firestore.document('projects/{projectId}')
 
+    .onDelete(snapshot => index.deleteObject(snapshot.id));
