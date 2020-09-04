2d066bedfd3982ee52608a139e270fbdaa5b548b
Max Geller <mxgeller@gmail.com>
2020-09-03 18:44:42 -0700

    modernizing algolia search

--- functions/index.ts
+++ functions/index.ts
@@ -1,3 +1,4 @@
+
 const functions = require('firebase-functions');
 const algoliasearch = require('algoliasearch');
 
@@ -5,35 +6,17 @@ const APP_ID = functions.config().algolia.app;
 const ADMIN_KEY = functions.config().algolia.key;
 
 const client = algoliasearch(APP_ID, ADMIN_KEY);
-const index = client.initIndex('PROJECT');
+const ALGOLIA_INDEX_NAME =  'projects';
 
 
-exports.addToIndex = functions.firestore.document('projects/{projectId}')
-
-    .onCreate(snapshot => {
-
-        const data = snapshot.data();
-        const objectID = snapshot.id;
-
-        return index.addObject({
-            ...data,
-            objectID
-        });
-
-    });
-
-
-exports.updateIndex = functions.firestore.document('projects/{projectId}')
-
-    .onUpdate((change) => {
-        const newData = change.after.data();
-        const objectID = change.after.id;
-        return index.saveObject({
-            ...newData,
-            objectID
-        });
-    });
-
-exports.deleteFromIndex = functions.firestore.document('projects/{projectId}')
-
-    .onDelete(snapshot => index.deleteObject(snapshot.id));
+exports.onProjectCreated = functions.firestore.document('projects/{projectId}').onCreate((snap, context) => {
+    // Get the note document
+    const project = snap.data();
+  
+    // Add an 'objectID' field which Algolia requires
+    project.objectID = context.params.projectId;
+  
+    // Write to the algolia index
+    const index = client.initIndex(ALGOLIA_INDEX_NAME);
+    return index.saveObject(project);
+  });
