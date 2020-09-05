7a98d1f745e2b146975cb116aae99526a9363b7a
Max Geller <mxgeller@gmail.com>
2020-09-04 17:03:26 -0700

    updating firestore sync functions

--- functions/index.ts
+++ functions/index.ts
@@ -5,32 +5,39 @@ const APP_ID = functions.config().algolia.app;
 const ADMIN_KEY = functions.config().algolia.key;
 
 const client = algoliasearch(APP_ID, ADMIN_KEY);
-const index = client.initIndex('PROJECT');
+const PROJECT_INDEX = client.initIndex('PROJECT');
+const TASK_INDEX = client.initIndex('TASK');
+const FILE_INDEX = client.initIndex('FILE');
+const LOCAL_INDEX = client.initIndex('LOCAL');
+const UNION_INDEX = client.initIndex('UNION');
+const INDICES_INDEX = client.initIndex('INDICES');
+const CONTRACTORS_INDEX = client.initIndex('CONTRACTORS');
+const ITEM_DATABASE_INDEX = client.initIndex('ITEM_DATABASE');
 
-
-exports.addToIndex = functions.firestore.document('projects/{projectId}')
+// PROJECT METHODS
+exports.addProjectToIndex = functions.firestore.document('projects/{projectId}')
 
     .onCreate(snapshot => {
 
         const data = snapshot.data();
         const objectID = snapshot.id;
 
-        return index.saveObject({ ...data, objectID });
+        return PROJECT_INDEX.saveObject({ ...data, objectID });
 
     });
 
 
-exports.updateIndex = functions.firestore.document('projects/{projectId}')
+exports.updateProjectIndex = functions.firestore.document('projects/{projectId}')
 
     .onUpdate((change) => {
         const newData = change.after.data();
         const objectID = change.after.id;
-        return index.saveObject({ ...newData, objectID });
+        return PROJECT_INDEX.saveObject({ ...newData, objectID });
     });
 
-exports.deleteFromIndex = functions.firestore.document('projects/{projectId}')
+exports.deleteProjectFromIndex = functions.firestore.document('projects/{projectId}')
 
-    .onDelete(snapshot => index.deleteObject(snapshot.id));
+    .onDelete(snapshot => PROJECT_INDEX.deleteObject(snapshot.id));
 
 
 exports.onProjectCreated = functions.firestore.document('projects/{projectId}').onCreate((snap, context) => {
@@ -40,7 +47,60 @@ exports.onProjectCreated = functions.firestore.document('projects/{projectId}').
     // Add an 'objectID' field which Algolia requires
     project.objectID = context.params.projectId;
 
-    // Write to the algolia index
-    const index = client.initIndex('projects');
-    return index.saveObject(project);
-});    
+    // Write to the algolia PROJECT_INDEX
+    const PROJECT_INDEX = client.initIndex('projects');
+    return PROJECT_INDEX.saveObject(project);
+});
+
+
+
+
+// TASK METHODS
+exports.addTaskToIndex = functions.firestore.document('tasks/{taskId}')
+
+    .onCreate(snapshot => {
+
+        const data = snapshot.data();
+        const objectID = snapshot.id;
+
+        return TASK_INDEX.saveObject({ ...data, objectID });
+
+    });
+
+
+exports.updateTaskIndex = functions.firestore.document('tasks/{taskId}')
+
+    .onUpdate((change) => {
+        const newData = change.after.data();
+        const objectID = change.after.id;
+        return TASK_INDEX.saveObject({ ...newData, objectID });
+    });
+
+exports.deleteTaskFromIndex = functions.firestore.document('tasks/{taskId}')
+
+    .onDelete(snapshot => TASK_INDEX.deleteObject(snapshot.id));
+
+
+exports.onTaskCreated = functions.firestore.document('tasks/{taskId}').onCreate((snap, context) => {
+    // Get the note document
+    const task = snap.data();
+
+    // Add an 'objectID' field which Algolia requires
+    task.objectID = context.params.taskId;
+
+    // Write to the algolia TASK_INDEX
+    const TASK_INDEX = client.initIndex('tasks');
+    return TASK_INDEX.saveObject(task);
+});
+
+
+
+
+
+// LOCAL METHODS
+// UNION METHODS
+// INDICES METHODS
+
+// CONTRACTORS METHODS
+// ITEM_DATABASE METHODS
+// PROJECT METHODS
