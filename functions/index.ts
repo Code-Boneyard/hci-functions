f4fccd4c398c63fc2684d1c0fdf69f563557d822
Max Geller <mxgeller@gmail.com>
2020-09-05 21:43:58 -0700

    updating functions

--- functions/index.ts
+++ functions/index.ts
@@ -11,6 +11,9 @@ const FILE_INDEX = client.initIndex('FILE');
 const LOCAL_INDEX = client.initIndex('LOCAL');
 const UNION_INDEX = client.initIndex('UNION');
 const INDICES_INDEX = client.initIndex('INDICES');
+const PACKAGE_INDEX = client.initIndex('PACKAGE');
+const ESTIMATE_INDEX = client.initIndex('ESTIMATE');
+const CONTRACT_INDEX = client.initIndex('CONTRACT');
 const CONTRACTORS_INDEX = client.initIndex('CONTRACTORS');
 const ITEM_DATABASE_INDEX = client.initIndex('ITEM_DATABASE');
 
@@ -92,6 +95,132 @@ exports.onTaskCreated = functions.firestore.document('tasks/{taskId}').onCreate(
     const TASK_INDEX = client.initIndex('TASK');
     return TASK_INDEX.saveObject(task);
 });
+
+
+
+// PACKAGE METHODS
+exports.addPackageToIndex = functions.firestore.document('package/{packageId}')
+
+    .onCreate(snapshot => {
+
+        const data = snapshot.data();
+        const objectID = snapshot.id;
+
+        return PACKAGE_INDEX.saveObject({ ...data, objectID });
+
+    });
+
+
+exports.updatePackageIndex = functions.firestore.document('package/{packageId}')
+
+    .onUpdate((change) => {
+        const newData = change.after.data();
+        const objectID = change.after.id;
+        return PACKAGE_INDEX.saveObject({ ...newData, objectID });
+    });
+
+exports.deletePackageFromIndex = functions.firestore.document('package/{packageId}')
+
+    .onDelete(snapshot => PACKAGE_INDEX.deleteObject(snapshot.id));
+
+
+exports.onPackageCreated = functions.firestore.document('package/{packageId}').onCreate((snap, context) => {
+    // Get the note document
+    const package = snap.data();
+
+    // Add an 'objectID' field which Algolia requires
+    package.objectID = context.params.projectId;
+
+    // Write to the algolia PROJECT_INDEX
+    const PACKAGE_INDEX = client.initIndex('PACKAGE');
+    return PACKAGE_INDEX.saveObject(package);
+});
+
+
+// ESTIMATE METHODS
+
+exports.addEstimateToIndex = functions.firestore.document('estimates/{estimateId}')
+
+    .onCreate(snapshot => {
+
+        const data = snapshot.data();
+        const objectID = snapshot.id;
+
+        return ESTIMATE_INDEX.saveObject({ ...data, objectID });
+
+    });
+
+
+exports.updateTaskIndex = functions.firestore.document('estimates/{estimateId}')
+
+    .onUpdate((change) => {
+        const newData = change.after.data();
+        const objectID = change.after.id;
+        return ESTIMATE_INDEX.saveObject({ ...newData, objectID });
+    });
+
+exports.deleteEstimateFromIndex = functions.firestore.document('estimates/{estimateId}')
+
+    .onDelete(snapshot => ESTIMATE_INDEX.deleteObject(snapshot.id));
+
+
+exports.onEstimateCreated = functions.firestore.document('estimates/{estimateId}').onCreate((snap, context) => {
+    // Get the note document
+    const estimate = snap.data();
+
+    // Add an 'objectID' field which Algolia requires
+    estimate.objectID = context.params.taskId;
+
+    // Write to the algolia TASK_INDEX
+    const ESTIMATE_INDEX = client.initIndex('ESTIMATE');
+    return ESTIMATE_INDEX.saveObject(estimate);
+});
+
+
+
+
+
+// CONTRACTS METHODS
+
+exports.addContractsToIndex = functions.firestore.document('contracts/{contractId}')
+
+    .onCreate(snapshot => {
+
+        const data = snapshot.data();
+        const objectID = snapshot.id;
+
+        return CONTRACT_INDEX.saveObject({ ...data, objectID });
+
+    });
+
+
+exports.updateContractIndex = functions.firestore.document('contracts/{contractId}')
+
+    .onUpdate((change) => {
+        const newData = change.after.data();
+        const objectID = change.after.id;
+        return CONTRACT_INDEX.saveObject({ ...newData, objectID });
+    });
+
+exports.deleteTaskFromIndex = functions.firestore.document('contracts/{contractId}')
+
+    .onDelete(snapshot => CONTRACT_INDEX.deleteObject(snapshot.id));
+
+
+exports.onTaskCreated = functions.firestore.document('contracts/{contractId}').onCreate((snap, context) => {
+    // Get the note document
+    const contract = snap.data();
+
+    // Add an 'objectID' field which Algolia requires
+    contract.objectID = context.params.taskId;
+
+    // Write to the algolia TASK_INDEX
+    const CONTRACT_INDEX = client.initIndex('CONTRACT');
+    return CONTRACT_INDEX.saveObject(contract);
+});
+
+
+
 
 
 
